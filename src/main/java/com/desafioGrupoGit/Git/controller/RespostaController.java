00d0be0a45f7d80307b8dd8d3f52d30a4342304d
TansenSilva <tansensilva@gmail.com>
2022-08-31 16:10:30 -0300

    Crud

--- src/main/java/com/desafioGrupoGit/Git/controller/RespostaController.java
+++ src/main/java/com/desafioGrupoGit/Git/controller/RespostaController.java
@@ -1,7 +1,20 @@
 package com.desafioGrupoGit.Git.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.desafioGrupoGit.Git.model.RespostaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RespostaController {
+
+    @Autowired
+    private RespostaModel service;
+
+
 }
--- src/main/java/com/desafioGrupoGit/Git/repository/RespostaRepository.java
+++ src/main/java/com/desafioGrupoGit/Git/repository/RespostaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RespostaRepository implements JpaRepository<RespostaModel,Long> {
+public interface RespostaRepository extends JpaRepository<RespostaModel,Long> {
 }
