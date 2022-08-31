793a0e0be8230be7fcd39e01a2207b7c74eb8289
TansenSilva <tansensilva@gmail.com>
2022-08-31 16:04:56 -0300

    Crud

--- src/main/java/com/desafioGrupoGit/Git/model/RespostaModel.java
+++ src/main/java/com/desafioGrupoGit/Git/model/RespostaModel.java
@@ -1,4 +1,23 @@
 package com.desafioGrupoGit.Git.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "resposta")
 public class RespostaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String mensagem;
+
 }
